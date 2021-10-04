97e2593d4fe84872c59f2407eb0139981c5d891b
Muratcan Erdoğan <erdoganmcan@gmail.com>
2021-10-04 14:57:39 +0300

    User list added to page.

--- src/index.js
+++ src/index.js
@@ -42,6 +42,10 @@ io.on("connection", (socket) => {
         "message",
         generateMessage("Admin", `${user.username} has joined!`)
       );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
     callback();
   });
@@ -78,6 +82,10 @@ io.on("connection", (socket) => {
         "message",
         generateMessage("Admin", `${user.username} has left!`)
       );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
     }
   });
 });
